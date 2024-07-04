1b90b6d7d6fe039c7a6c02ecaa644a6127974b63
mgtadesse7 <mgtadesse7@gmail.com>
2024-07-04 00:26:34 -0500

    database config

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -4,7 +4,7 @@ const dbConnection = mysql2.createPool({
   socketPath: process.env.DB_SOCKETPATH,
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
-  host: "localhost",
+  host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   connectionLimit: 10,
 });
